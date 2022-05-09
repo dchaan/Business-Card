0cc8ab69fe45d1ef733f6e983465c0cdfe383f94
David Chan <dchaan1@gmail.com>
2022-05-08 21:35:51 -0700

    Finish

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import Info from './components/Info'
 import About from './components/About'
 import Interests from './components/Interests'
+import Footer from './components/Footer'
 import './App.css';
 import { render } from '@testing-library/react';
 
@@ -11,6 +12,7 @@ function App() {
       <Info />
       <About />
       <Interests />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className="footer-container">
+      <a href="https://docs.google.com/document/d/1M-WOUI9xJp7OEpOlJ9rPeTO8LHLFPM3PqM1C9r_6-dE/edit?usp=sharing" target="_blank" rel="noreferrer"><i className="fa fa-file-text-o"></i></a>
+      <a href="https://dchaan.github.io/Portfolio/" target="_blank" rel="noreferrer"><i className="fa fa-user"></i></a>
+      <a href="https://github.com/dchaan" target="_blank" rel="noreferrer"><i className="fa fa-github"></i></a>
+    </div>
+  )
+}
